3bce3fed21ba8d96503acd83c963993a41fc3346
Farman Haris <farmanharis66@gmail.com>
2024-10-31 22:17:28 +0500

    useState Method Intials

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import React, { useState } from 'react'
+
 const App = () => {
+  const [user, setUser] = useState(10)
+
+  const change_User = () => {
+    setUser(20)
+  }
   return (
-    <h1>Cyprus</h1>
+    <div>
+      <h2>Username is {user}</h2>
+      <button onClick={change_User}>Change User</button>
+    </div>
   )
 }
 
 export default App
+const user = "Farman"
+const age = 69
