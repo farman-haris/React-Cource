f7ada080f6fe3dbfce03e093a69fd96764381f54
Farman Haris <farmanharis66@gmail.com>
2024-10-31 22:25:21 +0500

    Couter using useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [user, setUser] = useState(10)
-
-  const change_User = () => {
-    setUser(20)
-  }
+  const [num, setNum] = useState(0)
   return (
     <div>
-      <h2>Username is {user}</h2>
-      <button onClick={change_User}>Change User</button>
+      <h3>Number is {num}</h3>
+      <button onClick={()=>{
+        setNum(num+10)
+      }}>Increament</button>
+      <button onClick={()=>{
+        setNum(num-10)
+      }}>Decreament</button>
     </div>
   )
 }
 
 export default App
-const user = "Farman"
-const age = 69
