fb29e5f3e9c1e0191b96eada26482c8368323efa
Farman Haris <farmanharis66@gmail.com>
2024-11-05 23:13:48 +0500

    Testing of Two way binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import React, { useState } from 'react'
 
 const App = () => {
-
   const [username, setUsername] = useState('')
-  
-  const SubmitHandler = (e)=>{
+
+  const submitHandler = (e) => {
     e.preventDefault()
-
     console.log(username);
-
+    
     setUsername('')
   }
   return (
     <div>
       <form onSubmit={(e)=>{
-        SubmitHandler(e)
+        submitHandler(e)
       }}>
         <input
         value={username}
-        onChange={(e)=> {
+        onChange={(e)=>{
           setUsername(e.target.value)
         }}
-        className='px-4 py-3 text-xl m-5 bg-white text-black' type="text" placeholder='Enter your name' />
-        <button className='px-4 py-3 text-xl font-semibold bg-emerald-600 rounded'>Submit</button>
+        className='py-4 px-3 text-xl m-5 bg-white text-black rounded' type="text" placeholder='Enter Your Name' />
+        <button className='py-4 px-3 text-xl m-5 bg-emerald-500 font-semibold rounded'>Submit</button>
       </form>
     </div>
   )
