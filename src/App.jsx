45f3fb80bf21f385bcff33add1aba28c9b4e2d01
Farman Haris <farmanharis66@gmail.com>
2024-11-05 22:58:06 +0500

    Two way binding, in this chapter: We created a useState and a submit handler outside the return function We use on submit in form inside return to submit the form and onChange keyword in input field to convert the text

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
+  const [username, setUsername] = useState('')
+  
   const SubmitHandler = (e)=>{
     e.preventDefault()
 
-    console.log("submitted");
-    
+    console.log(username);
+
+    setUsername('')
   }
   return (
     <div>
       <form onSubmit={(e)=>{
         SubmitHandler(e)
       }}>
-        <input className='px-4 py-3 text-xl m-5 bg-white text-black' type="text" placeholder='Enter your name' />
+        <input
+        value={username}
+        onChange={(e)=> {
+          setUsername(e.target.value)
+        }}
+        className='px-4 py-3 text-xl m-5 bg-white text-black' type="text" placeholder='Enter your name' />
         <button className='px-4 py-3 text-xl font-semibold bg-emerald-600 rounded'>Submit</button>
       </form>
     </div>
