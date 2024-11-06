431cbeb3fac2c6c32e2a36afa50e14015df59e92
Farman Haris <farmanharis66@gmail.com>
2024-11-06 22:03:38 +0500

    Components, Initials

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
-  const [username, setUsername] = useState('')
-
-  const submitHandler = (e) => {
-    e.preventDefault()
-    console.log(username);
-    
-    setUsername('')
-  }
   return (
-    <div>
-      <form onSubmit={(e)=>{
-        submitHandler(e)
-      }}>
-        <input
-        value={username}
-        onChange={(e)=>{
-          setUsername(e.target.value)
-        }}
-        className='py-4 px-3 text-xl m-5 bg-white text-black rounded' type="text" placeholder='Enter Your Name' />
-        <button className='py-4 px-3 text-xl m-5 bg-emerald-500 font-semibold rounded'>Submit</button>
-      </form>
-    </div>
+    <>
+    <Navbar />
+    <Footer />
+    </>
   )
 }
 
